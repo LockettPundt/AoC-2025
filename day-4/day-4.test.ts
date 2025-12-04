310b8c83188543a0c6be27ad8f421935edbafa06
Lockett Pundt <kalifornium@hotmail.com>
2025-12-04 11:59:57 -0500

    feat(Advent of Code): :sparkles: part two

--- day-4/day-4.test.ts
+++ day-4/day-4.test.ts
@@ -4,8 +4,13 @@ import { printingDepartment } from "./day-4.ts";
 import { splitByNewLine } from "../utils/utils.ts"
 
 describe("Day 4", () => { 
-  const data = splitByNewLine("day-4/test-data.txt");
+  const data = splitByNewLine("day-4/test-data.txt").map(x => x.split(""));
   test("Part One", () => {
+
     strictEqual(printingDepartment(data), 13); // * 1395
+  });
+
+  test("Part Two", () => {
+    strictEqual(printingDepartment(data, { removePaperRolls: true, total: 0 }), 43); // * 8451
   });
 });
--- day-4/day-4.ts
+++ day-4/day-4.ts
@@ -1,28 +1,60 @@
-export const printingDepartment = (input: string[]) => {
+export const printingDepartment = (
+  input: string[][],
+  partTwo: {
+    removePaperRolls: boolean;
+    total: number;
+  } = {
+    removePaperRolls: false,
+    total: 0,
+  }
+) => {
   const paperRoll = "@";
-  return input.reduce((paperTotal, gridRow, gridIndex, gridArray) => {
-    return (paperTotal += gridRow
-      .split("")
-      .reduce((rowSum, current, rowIndex, rowArray) => {
-        if (current === paperRoll) {
-          const adjacentPositions = [
-            ...((gridArray?.[gridIndex - 1] ?? "")
-              .split("")
-              .slice(rowIndex > 0 ? rowIndex - 1 : rowIndex, rowIndex + 2) ??
-              []), // * above
-            rowArray?.[rowIndex + 1], // * right
-            rowArray?.[rowIndex - 1], // * left
-            ...((gridArray?.[gridIndex + 1] ?? "")
-              .split("")
-              .slice(rowIndex > 0 ? rowIndex - 1 : rowIndex, rowIndex + 2) ??
-              []), // * below
-          ].filter((x) => x === paperRoll);
+  let paperTotal = 0;
+  const { removePaperRolls, total } = partTwo;
 
-          if (adjacentPositions.length < 4) {
-            rowSum += 1;
+  let grid = input;
+
+  for (let gridIndex = 0; gridIndex < grid.length; gridIndex++) {
+    let rowSum = 0;
+    let gridRow = grid[gridIndex];
+
+    for (let rowIndex = 0; rowIndex < gridRow.length; rowIndex++) {
+      const current = gridRow[rowIndex];
+      if (current === paperRoll) {
+        const adjacentPositions = [
+          ...((grid?.[gridIndex - 1] ?? []).slice(
+            rowIndex > 0 ? rowIndex - 1 : rowIndex,
+            rowIndex + 2
+          ) ?? []), // * above
+          gridRow?.[rowIndex + 1] ?? "out of bounds right", // * right
+          gridRow?.[rowIndex - 1] ?? "out of bounds left", // * left
+          ...((grid?.[gridIndex + 1] ?? []).slice(
+            rowIndex > 0 ? rowIndex - 1 : rowIndex,
+            rowIndex + 2
+          ) ?? []), // * below
+        ].filter((x) => x === paperRoll);
+
+        if (adjacentPositions.length < 4) {
+          rowSum += 1;
+          if (removePaperRolls) {
+            grid[gridIndex].splice(rowIndex, 1, "x");
           }
         }
-        return rowSum;
-      }, 0));
-  }, 0);
+      }
+    }
+    paperTotal += rowSum;
+  }
+
+  if (removePaperRolls) {
+    if (paperTotal === 0) {
+      return total;
+    } else {
+      return printingDepartment(grid, {
+        removePaperRolls: true,
+        total: paperTotal + total,
+      });
+    }
+  }
+
+  return paperTotal;
 };
